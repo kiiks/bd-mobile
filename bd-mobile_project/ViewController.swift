71242677d1c6eb681e0c49bd4ad1b1aa1b746cf8
Killian DROULEZ <killian.droulez@outlook.fr>
2021-02-24 09:30:26 +0100

    Initial Commit

---
+++ bd-mobile_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bd-mobile_project
+//
+//  Created by Killian DROULEZ on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
